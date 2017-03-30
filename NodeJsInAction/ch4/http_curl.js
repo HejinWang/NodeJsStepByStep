0890fa3b4e183ab1cbf4aa0d7dcaa11df20c795f
Anduril <egger@outlook.com>
2017-03-30 23:40:43 +0800

    curl   delete put  implemented!

--- NodeJsInAction/ch4/http_curl.js
+++ NodeJsInAction/ch4/http_curl.js
@@ -15,6 +15,24 @@ var server = http.createServer(function (req, res) {
         res.end('OK\n');
       });
       break;
+    case 'PUT':
+      //curl -X PUT  http://localhost:3000/1?foo=foo&bar=bar
+      //curl -X PUT  -d "foo=foo&bar=bar" http://localhost:3000/1
+      var res_url = url.parse(req.url);
+      var path = res_url.pathname;
+      var i = parseInt(path.slice(1), 10);
+      if (isNaN(i)) {  //检查数字是否有效    
+        res.statusCode = 400;
+        res.end('Invalid item id');
+      } else if (!items[i]) {  //确保请求的索引存在   
+        res.statusCode = 404;
+        res.end('Item not found');
+      } else {
+        //更新内容
+        items[i] = res_url.query;
+        res.end('OK\n');
+      };
+      break;
     case 'GET':
       var body = items.map(function (item, i) {
         //res.write(i + ') ' + item + '\n');
@@ -25,6 +43,7 @@ var server = http.createServer(function (req, res) {
       res.end(body);
       break;
     case 'DELETE':  //在switch语句中添加DELETE case  
+      //curl -I -X DELETE http://localhost:3000/1?api-key=foobar
       var path = url.parse(req.url).pathname;
       var i = parseInt(path.slice(1), 10);
       if (isNaN(i)) {  //检查数字是否有效    
--- NodeJsInAction/ch4/readstream_static.js
+++ NodeJsInAction/ch4/readstream_static.js
@@ -1,18 +1,19 @@
+//静态文件服务器
 var http = require('http');
 var parse = require('url').parse;
 var join = require('path').join;
 var fs = require('fs');
 
-var root = __dirname;
+var root = __dirname;  //__dirname是该文件所在目录的路径
 
-var server = http.createServer(function(req, res){
+var server = http.createServer(function (req, res) {
   var url = parse(req.url);
   var path = join(root, url.pathname);
   var stream = fs.createReadStream(path);
-  stream.on('data', function(chunk){
+  stream.on('data', function (chunk) {
     res.write(chunk);
   });
-  stream.on('end', function(){
+  stream.on('end', function () {
     res.end();
   });
 });
