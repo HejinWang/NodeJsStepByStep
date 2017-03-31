2b8666c20476daecc03ffb367e44ed9a4620775b
Anduril <egger@outlook.com>
2017-03-31 23:43:06 +0800

    TODO page  not ok

--- NodeJsInAction/ch4/http_get_post.js
+++ NodeJsInAction/ch4/http_get_post.js
@@ -1,7 +1,8 @@
 var http = require('http');
+var qs = require('querystring');
 var items = [];
 
-var server = http.createServer(function(req, res){
+var server = http.createServer(function (req, res) {
   if ('/' == req.url) {
     switch (req.method) {
       case 'GET':
@@ -17,5 +18,44 @@ var server = http.createServer(function(req, res){
     notFound(res);
   }
 });
+function add(req, res) {
+  var body = '';
+  req.setEncoding('utf8');
+  req.on('data', function (chunk) {
+    body += chunk
+  });
+  req.on('end', function () {
+    var obj = qs.parse(body);
+    items.push(obj.item);
+    show(res);
+  });
+}
+function show(res) {
+  var html = '<html><head><title>Todo List</title></head><body>'
+    + '<h1>Todo List</h1>'//对简单的程序而言，用嵌入的HTML取代模板引擎一样好用        
+    + '<ul>'
+    + items.map(function (item) {
+      return '<li>' + item + '</li>'
+    }).join('')
+    + '</ul>'
+    + '<form method="post" action="/">'
+    + '<p><input type="text" name="item" /></p>'
+    + '<p><input type="submit" value="Add Item" /></p>'
+    + '</form></body></html>';
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Length', Buffer.byteLength(html));
+  res.end(html);
+}
 
+function notFound(res) {
+  res.statusCode = 404;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Not Found');
+}
+
+function badRequest(res) {
+  res.statusCode = 400;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Bad Request');
+}
 server.listen(3000);
--- NodeJsInAction/ch4/readstream_static.js
+++ NodeJsInAction/ch4/readstream_static.js
@@ -6,16 +6,41 @@ var fs = require('fs');
 
 var root = __dirname;  //__dirname是该文件所在目录的路径
 
-var server = http.createServer(function (req, res) {
+// var server = http.createServer(function (req, res) {
+//   var url = parse(req.url);
+//   var path = join(root, url.pathname);
+//   var stream = fs.createReadStream(path);
+//   stream.on('data', function (chunk) {
+//     res.write(chunk);
+//   });
+//   stream.on('end', function () {
+//     res.end();
+//   });
+// });
+
+var server = http.createServer(function(req, res){
   var url = parse(req.url);
   var path = join(root, url.pathname);
-  var stream = fs.createReadStream(path);
-  stream.on('data', function (chunk) {
-    res.write(chunk);
-  });
-  stream.on('end', function () {
-    res.end();
+  fs.stat(path, function(err, stat){
+    if (err) {
+      if ('ENOENT' == err.code) {
+        res.statusCode = 404;
+        res.end('Not Found');
+      } else {
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      }
+    } else {
+      res.setHeader('Content-Length', stat.size);
+      var stream = fs.createReadStream(path);
+      stream.pipe(res);
+      stream.on('error', function(err){
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      });
+    }
   });
 });
 
 server.listen(3000);
+//curl http://localhost:3000/file_server.js  -i
